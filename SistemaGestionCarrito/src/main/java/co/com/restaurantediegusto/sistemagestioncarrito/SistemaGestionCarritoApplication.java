9233625efe4767dc814f30524df1ef7e59261604
ZunoSuninsqui <juanpisave27@gmail.com>
2025-11-13 09:34:04 -0500

    primer push

---
+++ SistemaGestionCarrito/src/main/java/co/com/restaurantediegusto/sistemagestioncarrito/SistemaGestionCarritoApplication.java
@@ -0,0 +1,13 @@
+package co.com.restaurantediegusto.sistemagestioncarrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionCarritoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestionCarritoApplication.class, args);
+    }
+
+}
---
+++ SistemaGestionCarrito/src/test/java/co/com/restaurantediegusto/sistemagestioncarrito/SistemaGestionCarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.restaurantediegusto.sistemagestioncarrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionCarritoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
